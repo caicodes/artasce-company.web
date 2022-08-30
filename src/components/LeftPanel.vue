139a1fe3d19527f8da9e7408acb0df721eb1b2c8
caicodes <caicodes1@gmail.com>
2022-08-30 15:36:53 -0600

    login form added, left panel adjusted

--- src/components/LeftPanel.vue
+++ src/components/LeftPanel.vue
@@ -9,17 +9,15 @@
       class="blob blob2 absolute rounded-full w-[764px] h-[764px] bg-secondary/10"
     ></div>
     <div class="left-inner">
-      <h5 class="uppercase font-light mt-8">Artasce Creative</h5>
-      <h1 class="uppercase font-thin text-5xl my-8">Company Portal</h1>
-      <h5 class="max-w-[80%] mx-auto font-light pb-12 text-3xl">
-        Partner Access
-      </h5>
-      <p class="max-w-[60%] mx-auto pb-4 mb-16">
-        Access your client portals, online store admin areas, firebase consoles
-        or partner control panels from this Artasce Creative login. For more
-        information, contact us!
-      </p>
-      <button class="btn btn-primary btn-lg font-light">Learn More</button>
+      <h1 class="uppercase font-thin text-5xl text-accent mb-4 opacity-25">
+        Company Portal
+      </h1>
+
+      <Login />
     </div>
   </div>
 </template>
+
+<script setup>
+import Login from "./app/Login.vue"
+</script>
---
+++ src/components/app/Login.vue
@@ -0,0 +1,43 @@
+<template>
+  <div
+    class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 mx-auto"
+  >
+    <div class="card-body">
+      <h5 class="uppercase font-light">Artasce Creative</h5>
+      <p class="max-w-sm mx-auto p-2 text-left font-light">
+        Access your company portals with Artasce Creative.
+      </p>
+      <div class="form-control">
+        <label class="label">
+          <span class="label-text">Email</span>
+        </label>
+        <input type="text" placeholder="email" class="input input-bordered" />
+      </div>
+      <div class="form-control">
+        <label class="label">
+          <span class="label-text">Password</span>
+        </label>
+        <input
+          type="text"
+          placeholder="password"
+          class="input input-bordered"
+        />
+        <label class="label">
+          <a href="#" class="label-text-alt link link-hover"
+            >Forgot password?</a
+          >
+        </label>
+      </div>
+      <div class="form-control mt-6">
+        <button class="btn btn-primary">Login</button>
+
+        <a
+          href="http://artascecreative.com"
+          target="_blank"
+          class="btn btn-link"
+          >Need Help?</a
+        >
+      </div>
+    </div>
+  </div>
+</template>
