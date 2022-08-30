ca89ab75a5e0d4acaaa072eec4528c1a3d219977
caicodes <caicodes1@gmail.com>
2022-08-29 18:06:51 -0600

    attempting to resolve vanilla build warning

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
 import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin"
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vanillaExtractPlugin()],
 })
